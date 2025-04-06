78c87b657f9ea2f29ef4da5203d2c703c88cdbad
Jorge Galleguillos <jorgegp@live.cl>
2025-04-05 22:05:28 -0300

    refactor(errors): Se agrego logger y manejo de errores

--- caja18-prueba-tecnica/Controllers/DeviceController.cs
+++ caja18-prueba-tecnica/Controllers/DeviceController.cs
@@ -1,29 +1,52 @@
 ﻿using caja18_prueba_tecnica.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace caja18_prueba_tecnica.Controllers
 {
     public class DeviceController : Controller
     {
         private readonly IDeviceService _deviceService;
+        private readonly ILogger<DeviceController> _logger;
 
-        public DeviceController(IDeviceService deviceService)
+        public DeviceController(IDeviceService deviceService, ILogger<DeviceController> logger)
         {
             _deviceService = deviceService;
+            _logger = logger;
         }
 
         public async Task<IActionResult> Index()
         {
-            var devices = await _deviceService.GetDevicesAsync();
-            return View(devices);
+            try
+            {
+                var devices = await _deviceService.GetDevicesAsync();
+                return View(devices);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al obtener los dispositivos en el controlador.");
+                ViewBag.ErrorMessage = "Ocurrió un error al obtener los dispositivos. Por favor, intente más tarde.";
+                return View("Error");
+            }
         }
 
         public async Task<IActionResult> Details(string id)
         {
-            var device = await _deviceService.GetDeviceByIdAsync(id);
-            if (device == null) return NotFound();
-            return View(device);
+            try
+            {
+                var device = await _deviceService.GetDeviceByIdAsync(id);
+                if (device == null)
+                {
+                    return NotFound();
+                }
+                return View(device);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error al obtener el dispositivo con ID: {id} en el controlador.");
+                ViewBag.ErrorMessage = "Ocurrió un error al obtener el dispositivo. Por favor, intente más tarde.";
+                return View("Error");
+            }
         }
     }
-
 }
--- caja18-prueba-tecnica/Repositories/DeviceRepository.cs
+++ caja18-prueba-tecnica/Repositories/DeviceRepository.cs
@@ -1,31 +1,64 @@
 ﻿using caja18_prueba_tecnica.Models;
 using caja18_prueba_tecnica.Repositories.Interfaces;
 using caja18_prueba_tecnica.Utils;
-using System.Net.Http;
 
 namespace caja18_prueba_tecnica.Repositories
 {
     public class DeviceRepository : IDeviceRepository
     {
         private readonly HttpClient _httpClient;
-
-        public DeviceRepository(HttpClient httpClient)
+        private readonly ILogger<DeviceRepository> _logger;
+        public DeviceRepository(HttpClient httpClient, ILogger<DeviceRepository> logger)
         {
             _httpClient = httpClient;
+            _logger = logger;
         }
 
         public async Task<IEnumerable<Device>> GetAllAsync()
         {
-            var response = await _httpClient.GetAsync("objects");
-            response.EnsureSuccessStatusCode();
-            return (await JsonUtils.DeserializeAsync<IEnumerable<Device>>(response.Content))!;
+            try
+            {
+                var response = await _httpClient.GetAsync("objects");
+                response.EnsureSuccessStatusCode();
+
+                var devices = await JsonUtils.DeserializeAsync<IEnumerable<Device>>(response.Content);
+    
+                return devices ?? new List<Device>();
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, "Error al realizar la solicitud HTTP para obtener los dispositivos.");
+                throw new Exception("Error al obtener dispositivos desde la API externa.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error inesperado al obtener los dispositivos.");
+                throw new Exception("Error inesperado al obtener dispositivos.");
+            }
         }
+
 
         public async Task<Device?> GetByIdAsync(string id)
         {
-            var response = await _httpClient.GetAsync($"objects/{id}");
-            if (!response.IsSuccessStatusCode) return null;
-            return await JsonUtils.DeserializeAsync<Device>(response.Content);
+            try
+            {
+                var response = await _httpClient.GetAsync($"objects/{id}");
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+                return await JsonUtils.DeserializeAsync<Device>(response.Content);
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, $"Error al realizar la solicitud HTTP para obtener el dispositivo con ID: {id}");
+                throw new Exception("Error al obtener el dispositivo desde la API externa.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error inesperado al obtener el dispositivo.");
+                throw new Exception("Error inesperado al obtener el dispositivo.");
+            }
         }
     }
 }
